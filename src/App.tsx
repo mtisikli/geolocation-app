8976ead0d7d1a99363129068019f0d92d13f8622
Mehmet Tansu Işıklı <mtisikli@gmail.com>
2022-09-12 20:36:11 +0300

    feat: Add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Map from "./components/Map/Map";
 
 const App = () => {
   return (
-    <div className="h-96">
-      <p className="font-serif text-3xl text-center">
+    <div className="h-screen">
+      <p className="font-sans text-4xl text-center py-4 text-gray-500">
         Airsiders Frontend Challenge
       </p>
       <Map />
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,22 +1,106 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, FeatureGroup, Popup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
+import { useEffect, useRef, useState } from "react";
+import axios, { AxiosError } from "axios";
+import osmtogeojson from "osmtogeojson";
+import useOnClickOutside from "../../hooks/useOutsideAlerter";
+import "leaflet-draw/dist/leaflet.draw.css";
 
 const Map = () => {
+  const [bBox, setBBox] = useState<string | null>(null);
+  const [geoJsonData, setGeoJsonData] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | unknown | null>(null);
+  const wrapperRef = useRef(null);
+
+  console.log(geoJsonData);
+
+  const clickOutsideHandler = () => {
+    if (bBox) setBBox(null);
+  };
+
+  useOnClickOutside(wrapperRef, clickOutsideHandler);
+
+  useEffect(() => {
+    if (bBox) {
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const response = await axios.get(
+            `https://www.openstreetmap.org/api/0.6/map?bbox=${bBox}`
+          );
+          setGeoJsonData(osmtogeojson(response.data));
+        } catch (error) {
+          const { response } = error as AxiosError;
+          setError(response?.data);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchData();
+    }
+  }, [bBox]);
+
+  const handleCreatePolygon = (e: any) => {
+    const polygonBBOX = e.layer.getBounds().toBBoxString();
+    setBBox(polygonBBOX);
+  };
+
+  const handleDeletePolygon = () => {
+    setBBox(null);
+  };
+
   return (
     <MapContainer
       center={[51.505, -0.09]}
-      zoom={6}
+      zoom={20}
       scrollWheelZoom={false}
-      className="h-96"
+      className="h-5/6"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <FeatureGroup>
+        <EditControl
+          position="topright"
+          onCreated={handleCreatePolygon}
+          onDeleted={handleDeletePolygon}
+          draw={{
+            polyline: false,
+            polygon: true,
+            rectangle: true,
+            marker: false,
+            circle: false,
+            circlemarker: false,
+          }}
+        />
+        <div ref={wrapperRef}>
+          <Popup className="h-96 overflow-scroll">
+            {loading && <p>Loading...</p>}
+            {typeof error === "string" && <p>{error}</p>}
+
+            {geoJsonData &&
+              geoJsonData.features.map((feature: any, index: number) => (
+                <div key={feature.id}>
+                  <p className="font-semibold">{index + 1}</p>
+                  <p>
+                    <span className="font-semibold">Type: </span>
+                    {feature.geometry.type}
+                  </p>
+                  <p className="font-semibold">Coordinates:</p>
+                  {feature.geometry.coordinates.map(
+                    (coordinate: any, index: number) => (
+                      <p key={index}>
+                        {coordinate[0]} {coordinate[1]}
+                      </p>
+                    )
+                  )}
+                </div>
+              ))}
+          </Popup>
+        </div>
+      </FeatureGroup>
     </MapContainer>
   );
 };
