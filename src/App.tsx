21ee99415d404e39e66865a68dee8f0ef7e6bfed
Mehmet Tansu Işıklı <mtisikli@gmail.com>
2022-09-12 08:57:33 +0300

    feat: Add map to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Map from "./components/Map/Map";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-96">
+      <p className="font-serif text-3xl text-center">
+        Airsiders Frontend Challenge
+      </p>
+      <Map />
     </div>
   );
-}
+};
 
 export default App;
